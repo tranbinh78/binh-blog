fca10b148ec271d99823aa6ce6d2d127a9f79281
tranbinh78 <tranbinhtlq12@gmail.com>
2025-11-29 22:59:28 +0700

    Create quiz-data.js

---
+++ quiz-data.js
@@ -0,0 +1,102 @@
+quizData["present-simple"] = [
+  {
+    question: "She ___ to school by bike every day.",
+    options: ["go", "goes", "is going", "going"],
+    answer: 1
+  },
+  {
+    question: "I usually ___ breakfast at 7 AM.",
+    options: ["have", "has", "am having", "having"],
+    answer: 0
+  },
+  {
+    question: "My parents ___ in a big company.",
+    options: ["works", "working", "work", "are work"],
+    answer: 2
+  },
+  {
+    question: "The sun ___ in the east.",
+    options: ["rise", "rises", "is rising", "rised"],
+    answer: 1
+  },
+  {
+    question: "He ___ coffee, he prefers tea.",
+    options: ["don’t drink", "doesn’t drink", "isn't drinking", "not drink"],
+    answer: 1
+  },
+  {
+    question: "We ___ English every Monday.",
+    options: ["study", "studies", "are study", "studying"],
+    answer: 0
+  },
+  {
+    question: "They ___ in that house.",
+    options: ["live", "lives", "are living", "living"],
+    answer: 0
+  },
+  {
+    question: "Tom ___ well with his classmates.",
+    options: ["get along", "get alongs", "gets along", "is get along"],
+    answer: 2
+  },
+  {
+    question: "My sister ___ French very well.",
+    options: ["speak", "speaks", "is speak", "speaking"],
+    answer: 1
+  },
+  {
+    question: "His job ___ a lot of traveling.",
+    options: ["require", "requires", "is requiring", "required"],
+    answer: 1
+  },
+  {
+    question: "Dogs ___ meat.",
+    options: ["eat", "eats", "are eating", "eated"],
+    answer: 0
+  },
+  {
+    question: "My friends often ___ to the cinema.",
+    options: ["go", "goes", "going", "are go"],
+    answer: 0
+  },
+  {
+    question: "The museum ___ at 8:00 AM.",
+    options: ["open", "opens", "is opening", "opened"],
+    answer: 1
+  },
+  {
+    question: "My brother never ___ late.",
+    options: ["is", "are", "be", "being"],
+    answer: 0
+  },
+  {
+    question: "Water ___ at 100°C.",
+    options: ["boil", "boils", "is boiling", "boiled"],
+    answer: 1
+  },
+  {
+    question: "We ___ a car, so we take the bus.",
+    options: ["don’t have", "doesn’t have", "not have", "aren’t have"],
+    answer: 0
+  },
+  {
+    question: "Anna ___ her homework after school.",
+    options: ["do", "does", "is doing", "doing"],
+    answer: 1
+  },
+  {
+    question: "My father ___ TV in the evening.",
+    options: ["watch", "watches", "is watching", "watching"],
+    answer: 1
+  },
+  {
+    question: "The Earth ___ around the Sun.",
+    options: ["move", "moves", "is moving", "moved"],
+    answer: 1
+  },
+  {
+    question: "He rarely ___ breakfast.",
+    options: ["eat", "eats", "is eat", "eating"],
+    answer: 1
+  }
+];
